528aebdd093f016bf23ebec6356483a285c816e0
Nicholas Myers <nic338@gmail.com>
2023-06-16 09:53:34 -0400

    joel fixed the conflict

---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+
+    }
+}
