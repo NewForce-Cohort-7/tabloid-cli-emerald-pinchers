15cba46955b43bd520a3e821a5921fcfaf8d2184
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-06-16 10:30:11 -0400

    initial post stuff

---
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class PostManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+        private string _connectionString;
+
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _postRepository = new PostRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Post Menu");
+            Console.WriteLine(" 1) List Posts");
+            Console.WriteLine(" 2) Post Details");
+            Console.WriteLine(" 3) Add Post");
+            Console.WriteLine(" 4) Edit Post");
+            Console.WriteLine(" 5) Remove Post");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "2":
+                    Post post = Choose();
+                    if (post == null)
+                    {
+                        return this;
+                    }
+                    else
+                    {
+                        return new PostDetailManager(this, _connectionString, post.Id);
+                    }
+                case "3":
+                    Add();
+                    return this;
+                case "4":
+                    Edit();
+                    return this;
+                case "5":
+                    Remove();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("!--- INVALID SELECTION ---!");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            Console.WriteLine();
+            List<Post> posts = _postRepository.GetAll();
+            foreach (Post post in posts)
+            {
+                Console.WriteLine(post);
+            }
+            Console.WriteLine();
+        }
+
+        private Post Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Post:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Post> posts = _postRepository.GetAll();
+
+            for (int i = 0; i < posts.Count; i++)
+            {
+                Post post = posts[i];
+                Console.WriteLine($" {i + 1}) {post.Title}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return posts[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("!--- INVALID SELECTION ---!");
+                Console.WriteLine();
+                return null;
+            }
+        }
+
+        private void Add()
+        {
+            Console.WriteLine();
+            Console.WriteLine("New Post");
+            Post post = new Post();
+
+            Console.WriteLine();
+            Console.Write("Title: ");
+            post.Title = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Url: ");
+            post.Url = Console.ReadLine();
+
+            _postRepository.Insert(post);
+        }
+
+        private void Edit()
+        {
+            Console.WriteLine();
+            Post postToEdit = Choose("Which post would you like to edit?");
+            if (postToEdit == null)
+            {
+                return;
+            }
+
+            Console.WriteLine();
+            Console.Write("New Title (blank to leave unchanged: ");
+            string title = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                postToEdit.Title = title;
+            }
+
+            Console.WriteLine();
+            Console.Write("New URL (blank to leave unchanged: ");
+            string url = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(url))
+            {
+                postToEdit.Url = url;
+            }
+
+
+            _postRepository.Update(postToEdit);
+        }
+
+        private void Remove()
+        {
+            Console.WriteLine();
+            Post postToDelete = Choose("Which post would you like to remove?");
+            if (postToDelete != null)
+            {
+                _postRepository.Delete(postToDelete.Id);
+                Console.WriteLine();
+                Console.WriteLine($"!--- {postToDelete.Title} has been DELETED ---!");
+                Console.WriteLine();
+            }
+        }
+    }
+}
